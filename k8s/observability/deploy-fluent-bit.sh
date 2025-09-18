a24f3d02aa156866d8d14bef1465e107473ead67
Alice Tomaz <tomaz.alice@gmail.com>
2025-09-17 23:48:29 -0300

    chore: removing observability code (go to another project

--- k8s/observability/deploy-fluent-bit.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-# Apply the configuration
-kubectl apply -f fluent-bit-config.yaml
-
-# Deploy Fluent Bit DaemonSet
-kubectl apply -f https://raw.githubusercontent.com/fluent/fluent-bit-kubernetes-logging/master/fluent-bit-ds.yaml
